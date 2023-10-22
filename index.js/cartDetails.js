f256058b0317aabaa7a4fccb6c0aadc3057d9b57
Ahmedbnk <145155073+Ahmedbnk@users.noreply.github.com>
2023-10-22 18:41:29 +0100

    Add files via upload

---
+++ index.js/cartDetails.js
@@ -0,0 +1,10 @@
+let products = JSON.parse(localStorage.getItem("productsData"));
+let productId = localStorage.getItem("productId");
+let itemDom = document.querySelector(".item-details");
+let productDetails = products.find((item) => item.id == productId);
+
+itemDom.innerHTML = `
+        <img src="${productDetails.imageUrl}" alt="image">
+        <h2>${productDetails.title}</h2>
+        Size: <span>${productDetails.size}</span><br>
+        Description:<p class="longDesc"> ${productDetails.longDesc}</p>`;
